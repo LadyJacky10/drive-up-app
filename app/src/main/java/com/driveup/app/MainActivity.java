9bb5e9436bc63edd883022affa407e936ba4ebb0
Karl Dilumueni <cdilumueni@gmail.com>
2022-01-06 00:47:14 +0100

    New Change by Karl

--- app/src/main/java/com/driveup/app/MainActivity.java
+++ app/src/main/java/com/driveup/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.driveup.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Hello Anna", Toast.LENGTH_LONG).show();
+        // On cache la barre de titre
+        getSupportActionBar().hide();
+
+        // On crée une sous-procédure (Handler) dans laquelle on exécute une interface
+        // Runnable et un délais en milliseconde qui permet à la fin du décompte de lancer
+        // une autre écran interface (Login)
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Je lance l'interface de Login
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },6000);
+
+        //Toast.makeText(this, "Hello Anna", Toast.LENGTH_LONG).show();
     }
 
 }
