e3e9c5a04cd1e66a53491a2bcc0046e4b4dc7bc0
Karl Dilumueni <cdilumueni@gmail.com>
2022-01-06 10:33:11 +0100

    New Change by Karl

--- app/src/main/java/com/driveup/app/LoginActivity.java
+++ app/src/main/java/com/driveup/app/LoginActivity.java
@@ -2,13 +2,52 @@ package com.driveup.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText lg_username, lg_password;
+    Button bt_cancel, bt_validate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        // Initialisation des récupérations de variables de l'interface Login
+        lg_username = findViewById(R.id.login_username);
+        lg_password = findViewById(R.id.login_password);
+        bt_cancel = findViewById(R.id.login_cancel_button);
+        bt_validate = findViewById(R.id.login_validate_button);
+
+        // Evènement Clic sur bouton annuler
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lg_username.setText(null);
+                lg_password.setText(null);
+            }
+        });
+        // Evènement Clic sur bouton connexion
+        bt_validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = lg_username.getText().toString();
+                String password = lg_password.getText().toString();
+                if (username == "MONKAMBULA ANNE" && password == "12345678"){
+                    // On prépare une Intention d'ouverture de la fenêtre Accueil
+                    //Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    //startActivity(intent); // On lance l'interface Accueil grâce à l'intention préparée
+                    //finish();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Nom d\'utilisateur ou mot de passe incorrect", Toast.LENGTH_SHORT)
+                            .show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/driveup/app/MainActivity.java
+++ app/src/main/java/com/driveup/app/MainActivity.java
@@ -20,13 +20,14 @@ public class MainActivity extends AppCompatActivity {
         // On crée une sous-procédure (Handler) dans laquelle on exécute une interface
         // Runnable et un délais en milliseconde qui permet à la fin du décompte de lancer
         // une autre écran interface (Login)
+        // Cette classe contient la méthode postDelayed qui prend en paramètre l'enterface Runnable et le temps en milliseconde
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Je lance l'interface de Login
+                // Je lance l'interface de Login grâce à l'Intent initialisé avec les coordonnées de changement.
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                startActivity(intent); // Je lance effectivement la fenêtre Login
+                finish(); // Disposer de la mémoire la précédente interface.
             }
         },6000);
 
